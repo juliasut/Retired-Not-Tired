55f20e64bfe9199c5fb8b2ce8cc36cb2b840e738
Dan Rees <danrees8743@gmail.com>
2022-11-08 21:31:56 +0000

    feat[home]: added pages folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import Home from './pages/Home';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Error from './pages/Error';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <Home />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>This is a login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
