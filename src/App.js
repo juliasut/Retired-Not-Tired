dd00bf94f9761991c6a8ee958583056893fa9bf0
Tami Gaertner <31107704+tagaertner@users.noreply.github.com>
2022-11-10 00:32:09 +0100

    feat-home-button-icon-image-calendar-logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login/Login';
-import Error from './pages/Page404/Page404';
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Error from "./pages/Page404/Page404";
 
 const queryClient = new QueryClient();
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,89 @@
-import { database } from "../config/firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import "./home.css";
+import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+import Calendar from "react-calendar";
+import Logo from "../../assets/images/retired-not-tired-just-flip-flops.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faListCheck,
+  faPeopleGroup,
+  faClock,
+  faCheck,
+  faHome,
+  faHeart,
+  faMessage,
+  faUserGroup,
+  faList,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  const [user, setUser] = useState(null);
-  const ref = collection(database, "user");
-
-  useEffect(() => {
-    const getUser = async () => {
-      const snapshot = await getDocs(ref);
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({ id: doc.id, ...doc.data() });
-      });
-      setUser(results);
-    };
-    getUser();
-  }, []);
+  const [date, setDate] = useState(new Date());
+  //  const element = <FontAwesomeIcon icon={faCoffee} />;
 
   return (
     <div>
-      //tami work here only! retard..
-      <h1>Retired Not Tired</h1>
-      {user &&
-        user.map((user) => (
-          <div key={user.id}>
-            {/* {console.log(user)} */}
-            <h2>
-              {user.firstName} <span>{user.lastName}</span>
-            </h2>
-            <p>{user.displayName}</p>
-          </div>
-        ))}
+      <div className="home">
+        <h2 className="title"> Stay Active</h2>
+        <h1 className="companyName">Retired Not Tired</h1>
+        <img src={Logo} alt="Retirement flip flop" />/<h1 className="header">Upcomming Activities</h1>
+        <h3 className="myActivies"> Activites on your schedule: </h3>
+        <div>
+          <button class="btn">
+            <FontAwesomeIcon icon={faListCheck}></FontAwesomeIcon>
+            unfulfilled task
+          </button>
+          <button class="btn">
+            <FontAwesomeIcon icon={faPeopleGroup}></FontAwesomeIcon>community request
+          </button>
+          <button class="btn">
+            <FontAwesomeIcon icon={faClock}></FontAwesomeIcon> hours avaiable to use
+          </button>
+          <button class="btn">
+            <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> completed hours
+          </button>
+        </div>
+        <div clossName="calendar-containter">
+          <Calendar onChange={setDate} vaule={date} />
+        </div>
+        <p className="text-center">
+          <span className="bold"> Selected Date: </span> {""}
+          {date.toDateString()}
+        </p>
+      </div>
+      <div>
+        <input type="checkbox" id="going" name="going" value="check" />
+        <lable for="going"> I am going</lable>
+        <input type="checkbox" id="interested" name="interested" value="check" />
+        <lable for="interested"> I am interested</lable>
+      </div>
+      <div>
+        <button class="btn"> Creat an activity</button>
+      </div>
+      <div>
+        <footer>
+          <button class="btn">
+            <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+            Home
+          </button>
+          <button class="btn">
+            <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+            Activities
+          </button>
+          <button class="btn">
+            <FontAwesomeIcon icon={faMessage}></FontAwesomeIcon>
+            Message
+          </button>
+        </footer>
+        <button class="btn">
+          <FontAwesomeIcon icon={faUserGroup}></FontAwesomeIcon>
+          Friends
+        </button>
+        <button class="btn">
+          <FontAwesomeIcon icon={faList}></FontAwesomeIcon>
+          More
+        </button>
+      </div>
     </div>
   );
 };
