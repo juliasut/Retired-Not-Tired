5bb081dc1a2a71bee72e81d2745a69a0d5e166e4
Tami Gaertner <31107704+tagaertner@users.noreply.github.com>
2022-11-12 10:15:28 +0100

    feat[home]:delete hard code and convert to react-mui created active cards with title and dates added css to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,96 +1,49 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import './home.css';
-import { useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
-import Calendar from 'react-calendar';
-import Logo from '../../assets/images/retired-not-tired-just-flip-flops.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faListCheck,
-  faPeopleGroup,
-  faClock,
-  faCheck,
-  faHome,
-  faHeart,
-  faMessage,
-  faUserGroup,
-  faList,
-} from '@fortawesome/free-solid-svg-icons';
+import "./home.css";
+// import { useState } from "react";
+import "react-calendar/dist/Calendar.css";
+// import Calendar from "react-calendar";
+import CalendarPicker from "@mui/x-date-pickers-pro/CalendarPicker";
+import Logo from "../../assets/images/retired-not-tired-just-flip-flops.png";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  const [date, setDate] = useState(new Date());
-  //  const element = <FontAwesomeIcon icon={faCoffee} />;
+// import Icon from "@mui/material/Icon";
+// const Home = () => {
+// const [date, setDate] = useState(new Date());
+//  const element = <FontAwesomeIcon icon={faCoffee} />;
+
+function Home() {
+  const dummyActivities = [
+    { id: 1, title: "swimming", date: new Date() },
+    { id: 2, title: "golf", date: new Date() },
+    { id: 3, title: "bingo", date: new Date() },
+    { id: 4, title: "boxing", date: new Date() },
+  ];
 
   return (
-    <div>
-      <div className="home">
-        <h2 className="title"> Stay Active</h2>
-        <h1 className="companyName">Retired Not Tired</h1>
-        <img src={Logo} alt="Retirement flip flop" />/
-        <h1 className="header">Upcomming Activities</h1>
-        <h3 className="myActivies"> Activites on your schedule: </h3>
-        <div>
-          <button class="btn">
-            <FontAwesomeIcon icon={faListCheck}></FontAwesomeIcon>
-            unfulfilled task
-          </button>
-          <button class="btn">
-            <FontAwesomeIcon icon={faPeopleGroup}></FontAwesomeIcon>community
-            request
-          </button>
-          <button class="btn">
-            <FontAwesomeIcon icon={faClock}></FontAwesomeIcon> hours avaiable to
-            use
-          </button>
-          <button class="btn">
-            <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> completed hours
-          </button>
-        </div>
-        <div clossName="calendar-containter">
-          <Calendar onChange={setDate} vaule={date} />
-        </div>
-        <p className="text-center">
-          <span className="bold"> Selected Date: </span> {''}
-          {date.toDateString()}
-        </p>
-      </div>
-      <div>
-        <input type="checkbox" id="going" name="going" value="check" />
-        <lable for="going"> I am going</lable>
-        <input
-          type="checkbox"
-          id="interested"
-          name="interested"
-          value="check"
-        />
-        <lable for="interested"> I am interested</lable>
-      </div>
-      <div>
-        <button class="btn"> Creat an activity</button>
-      </div>
-      <div>
-        <footer>
-          <button class="btn">
-            <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
-            Home
-          </button>
-          <button class="btn">
-            <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
-            Activities
-          </button>
-          <button class="btn">
-            <FontAwesomeIcon icon={faMessage}></FontAwesomeIcon>
-            Message
-          </button>
-        </footer>
-        <button class="btn">
-          <FontAwesomeIcon icon={faUserGroup}></FontAwesomeIcon>
-          Friends
-        </button>
-        <button class="btn">
-          <FontAwesomeIcon icon={faList}></FontAwesomeIcon>
-          More
-        </button>
+    <div className="container">
+      <Typography variant="h4" component="h3" align="center" gutterBottom>
+        Stay Active
+      </Typography>
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        Retrired Not Tired
+      </Typography>
+      <img className="logo" src={Logo} alt="Retirement flip flop" />
+      <Typography variant="h3" component="h2" align="center" gutterBottom>
+        Activites on your schedule:
+      </Typography>
+      <div className="activity-containter">
+        {dummyActivities.map((activity) => (
+          <Link to={`/activity/${activity.id}`}>
+            <Card sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }} key={activity.id}>
+              <p>{activity.title}</p>
+              <p>{format(activity.date, "dd-MM-yyyy")}</p>
+            </Card>
+          </Link>
+        ))}
       </div>
     </div>
   );
