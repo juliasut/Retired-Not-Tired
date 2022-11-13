e81dd68a62c3be3849a01b3d613c4e6446d904a7
juliasut <juliasutygina@gmail.com>
2022-11-13 11:03:53 -0700

    Resolve merge conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home.jsx';
+import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Error from './pages/Page404/Page404';
 
