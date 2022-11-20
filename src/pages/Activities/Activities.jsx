e2d230c4e781b8dba193246beac883dcde6318fc
juliasut <juliasutygina@gmail.com>
2022-11-20 06:15:20 -0700

    Activities page is using ActivityCard component to display data

--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -1,19 +1,27 @@
 import { useCollection } from '../../hooks/useCollection';
+import ActivityCard from '../../components/ActivityCard'
 
 const Activities = () => {
   const { documents, error } = useCollection('activities');
+  console.log(documents)
   return (
     <div>
       <h1>Activities</h1>
       <p>Activities page body content</p>
       {error && <p>{error}</p>}
-      {documents &&
+      {/* {documents &&
         documents.map((doc) => (
           <div key={doc.id}>
             <h2>{doc.title}</h2>
             <p>{doc.location}</p>
           </div>
-        ))}
+        ))} */}
+      {documents && documents.map(doc => (
+        <ActivityCard key={doc.id} title={doc.title} location={doc.location}
+          description={doc.description} 
+          contact={doc.contact}
+        />
+      ))}
     </div>
   );
 };
