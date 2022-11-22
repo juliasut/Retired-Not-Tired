e0647ff7043854a00a66a09bb5766b6f330b3c8f
juliasut <juliasutygina@gmail.com>
2022-11-22 01:58:21 -0700

    Rm unnecessary Link component around ActivityCard

--- src/components/ActivitiesList.jsx
+++ src/components/ActivitiesList.jsx
@@ -1,14 +1,11 @@
-import { Link } from 'react-router-dom';
 import ActivityCard from './ActivityCard';
 
-const ActivitiesList = ({ activity }) => {
+const ActivitiesList = ({ activities }) => {
   return (
     <div>
-      {activity.length === 0 && <p>No Activities yet</p>}
-      {activity.map((doc) => (
-        <Link to={`/activity-detail/${doc.id}`} key={doc.id}>
-          <ActivityCard activity={doc} key={doc.id} />
-        </Link>
+      {activities.length === 0 && <p>No Activities yet</p>}
+      {activities.map((activity) => (
+        <ActivityCard activity={activity} key={activity.id} />
       ))}
     </div>
   );
