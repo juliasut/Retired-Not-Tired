77f8e6bc373c09302bbab95f6d5db10bfbff181d
juliasut <juliasutygina@gmail.com>
2022-11-23 11:37:44 -0700

    Add className to style horizontal scroller, import css

--- src/components/ActivitiesList.jsx
+++ src/components/ActivitiesList.jsx
@@ -1,8 +1,9 @@
 import ActivityCard from './ActivityCard';
+import "./activities-list.css"
 
 const ActivitiesList = ({ activities }) => {
   return (
-    <div>
+    <div className='media-scroller'>
       {activities.length === 0 && <p>No Activities yet</p>}
       {activities.map((activity) => (
         <ActivityCard activity={activity} key={activity.id} />
