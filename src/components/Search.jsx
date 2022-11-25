156591e4f2f91f3834a46d9e3ea5e26e32c64bae
juliasut <juliasutygina@gmail.com>
2022-11-24 19:00:17 -0700

    Create reusable Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,32 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { InputBase, Stack } from '@mui/material';
+import theme from '../theme';
+
+const Search = ({ placeholder, onChange }) => {
+  return (
+    <Stack
+      alignSelf="center"
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      width="100%"
+      sx={{
+        p: '0 10px',
+        border: `1px solid ${theme.palette.textColor.secondary}`,
+        borderRadius: theme.shape.borderRadius,
+        [theme.breakpoints.up('sm')]: {
+          width: '300px',
+        },
+      }}
+    >
+      <InputBase
+        variant="outlined"
+        placeholder={placeholder}
+        onChange={onChange}
+      ></InputBase>
+      <SearchIcon />
+    </Stack>
+  );
+};
+
+export default Search;
