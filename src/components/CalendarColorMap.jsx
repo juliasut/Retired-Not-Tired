1524d5e647a36e3304884b2dd640b3a2632ff1c6
juliasut <juliasutygina@gmail.com>
2022-12-04 21:35:21 -0700

    Add component for color legend on calendar

---
+++ src/components/CalendarColorMap.jsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+export default function CalendarColorMap() {
+  return (
+    <Stack direction="row" width="320px" sx={{mb: 3, px: '15px'}}>
+        <Stack direction="row" gap={2} width="50%">
+          <Box
+            sx={{
+              background: '#625B75',
+              height: '22px',
+              width: '22px',
+              borderRadius: '5px',
+            }}
+          />
+          <Typography>I'm going</Typography>
+        </Stack>
+        <Stack direction="row" gap={2} width="50%">
+          <Box
+            sx={{
+              background: '#e5def6',
+              height: '22px',
+              width: '22px',
+              borderRadius: '5px',
+            }}
+          />
+          <Typography>I'm interested</Typography>
+        </Stack>
+      </Stack>
+  )
+}
